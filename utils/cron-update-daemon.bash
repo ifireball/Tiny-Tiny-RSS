7200f5d471f5ec7c5a3973f7b83c490d10ed734b
Barak Korren <barak.korren@gmail.com>
2013-03-26 19:35:41 +0200

    Added log rotation functionality to the update daemon monitoring script

--- utils/cron-update-daemon.bash
+++ utils/cron-update-daemon.bash
@@ -19,6 +19,10 @@ die() {
 	exit $ERROR
 }
 
+warn() {
+	echo WARNING: "$@" 1>&2
+}
+
 # Try to use the 'dirname' command and fall back to bash hacks if we can't
 DIRNAME='/usr/bin/dirname'
 if test -x $DIRNAME ; then
@@ -50,6 +54,11 @@ LOG_FILE="$SCRIPT_DIR/../logs/tt-rss-update-daemon.log"
 PID_FILE="$SCRIPT_DIR/../lock/${SCRIPT_NAME}.pid"
 NICE_LEVEL=10
 LOG_LINES=10
+ROTATE_LOG='yes'
+OLD_LOGS_TO_KEEP=5
+MAX_LOG_SIZE='500M'
+ROTATE_DURATION='weekly'
+LR_STATE_FILE="${LOG_FILE}.state"
 
 PHP="/usr/bin/php"
 NOHUP="/usr/bin/nohup"
@@ -57,6 +66,8 @@ NICE="/usr/bin/nice"
 TAIL="/usr/bin/tail"
 PS="/bin/ps"
 CAT="/bin/cat"
+MKTEMP="/bin/mktemp"
+LOGROTATE="/usr/sbin/logrotate"
 
 DAEMON="$SCRIPT_DIR/../update_daemon2.php"
 
@@ -64,13 +75,48 @@ DAEMON="$SCRIPT_DIR/../update_daemon2.php"
 test -r "$CONF_FILE" && source "$CONF_FILE"
 
 RUN_DIR="$(dirname "$DAEMON")"
+# Check log rotation parameters
+ROTATE_LOG="${ROTATE_LOG,,}"
+[[ "$ROTATE_LOG" == "yes" ]] || ROTATE_LOG=""
+(( OLD_LOGS_TO_KEEP="$OLD_LOGS_TO_KEEP" )) > /dev/null 2>&1 || OLD_LOGS_TO_KEEP=0
+[[ "$MAX_LOG_SIZE" =~ ^[0-9]+[KkMmGg]?$ ]] || MAX_LOG_SIZE=""
+ROTATE_DURATION="${ROTATE_DURATION,,}"
+[[ "$ROTATE_DURATION" =~ ^(daily|weekly|monthly|yearly)$ ]] || ROTATE_DURATION=""
+[[ -n "$MAX_LOG_SIZE" || -n "$ROTATE_DURATION" ]] || MAX_LOG_SIZE="500M"
 
 # Check to see if we have everything we need
-for CMD in $PHP $NOHUP $NICE $TAIL $PS; do
+for CMD in $PHP $NOHUP $NICE $TAIL $PS $CAT $MKTEMP; do
 	test -x $CMD || die -2 "Cannot find required command: $CMD"
 done
+# Since logrotate(8) may be less common then the rest look for it only if we
+# really need it, and fallback to not rotating if not found
+if [[ -n "$ROTATE_LOG" && ! -x $LOGROTATE ]]; then 
+	warn "Cannot find $LOGROTATE and user asked to rotate logs"
+	ROTATE_LOG=""
+fi
 
 test -r "$DAEMON" || die "TT-RSS update daemon not found at $DAEMON"
+
+# Rotate logs if configuration file said to do it
+if [[ -n "$ROTATE_LOG" ]]; then
+	TFILE=$($MKTEMP)
+	# Set trap to remove temp file even in case that a failure causes the
+	# shell to exit
+	trap "rm -f '$TFILE'" EXIT
+	(
+		echo "\"$LOG_FILE\" {"
+		echo "missingok"
+		echo "compress"
+		echo "rotate" $OLD_LOGS_TO_KEEP
+		echo "copytruncate"
+		[[ -n "$ROTATE_DURATION" ]] && echo $ROTATE_DURATION
+		[[ -n "$MAX_LOG_SIZE" ]] && echo "size" $MAX_LOG_SIZE
+		echo "}"
+	) > "$TFILE"
+	$LOGROTATE -s "$LR_STATE_FILE" "$TFILE"
+	# Delete temp file and remove trap on success
+	rm -f "$TFILE" && trap - EXIT
+fi
 
 # Check to see if daemon is aleady running, exit if it does
 test -r "$PID_FILE" &&
