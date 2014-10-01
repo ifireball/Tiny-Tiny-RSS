86311fbd5ac9fc5b8a4a850dc51667c10651be10
Barak Korren <barak.korren@gmail.com>
2014-10-01 23:58:32 +0300

    Fix DB configuration for MySQL

--- config.php
+++ config.php
@@ -4,11 +4,11 @@
 	// *******************************************
 
 	define('DB_TYPE', "mysql");
-	define('DB_HOST', getenv('OPENSHIFT_POSTGRESQL_DB_HOST'));
-        define('DB_USER', getenv('OPENSHIFT_POSTGRESQL_DB_USERNAME'));
+	define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+        define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
         define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
-        define('DB_PASS', getenv('OPENSHIFT_POSTGRESQL_DB_PASSWORD'));
-        define('DB_PORT', getenv('OPENSHIFT_POSTGRESQL_DB_PORT'));
+        define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+        define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
 
 	define('MYSQL_CHARSET', 'UTF8');
 	// Connection charset for MySQL. If you have a legacy database and/or experience
